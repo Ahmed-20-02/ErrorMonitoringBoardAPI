829f0bab09ac63081bc67d2bcc2c613ed63d22e4
Ahmed-20-02 <asaeed2002.as@gmail.com>
2024-03-13 17:23:30 +0000

    Dependency Injection
    Set up dependency injection

---
+++ DevelopmentProjectErrorBoardAPI/Business/AutofacDependencies/AutofacBusinessModule.cs
@@ -0,0 +1,18 @@
+namespace DevelopmentProjectErrorBoardAPI.Business.AutofacDependencies
+{
+    using System.Reflection;
+    using Autofac;
+
+    public class AutofacBusinessModule : Autofac.Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterAssemblyTypes(Assembly.GetExecutingAssembly())
+                .AsImplementedInterfaces();
+        }
+    }
+}
+
+//Reference
+//https://stackoverflow.com/questions/69754985/adding-autofac-to-net-core-6-0-using-the-new-single-file-template/71448702#71448702
+//13/03/2024
--- DevelopmentProjectErrorBoardAPI/Program.cs
+++ DevelopmentProjectErrorBoardAPI/Program.cs
@@ -1,4 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using DevelopmentProjectErrorBoardAPI.Business.AutofacDependencies;
+
 var builder = WebApplication.CreateBuilder(args);
+
+////Reference
+//https://stackoverflow.com/questions/69754985/adding-autofac-to-net-core-6-0-using-the-new-single-file-template/71448702#71448702
+//02/01/2024
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory())
+    .ConfigureContainer<ContainerBuilder>(builder =>
+    {
+        builder.RegisterModule(new AutofacBusinessModule());
+    });
 
 // Add services to the container.
 
